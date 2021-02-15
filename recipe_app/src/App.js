576ca790391d8576fcb56eed3cf2c3c2d94918bb
ProgBot12 <emir.compwiz@gmail.com>
2021-02-15 12:06:11 -0500

    fetch/API data + create Recipe component

--- recipe_app/src/App.js
+++ recipe_app/src/App.js
@@ -1,4 +1,6 @@
+import React, { useEffect, useState } from "react";
 import "./App.css";
+import Recipe from "./Components/Recipe";
 
 //App function component
 const App = () => {
@@ -6,12 +8,30 @@ const App = () => {
   const APP_ID = "2297f343";
   const APP_KEY = "50ce8dfaed3ee54874588ee166e66381";
 
-  const exampleReq = `https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free
-  `;
+  //state variable
+  const [recipes, setRecipes] = useState([]);
+
+  //fetch API data on first render
+  useEffect(() => {
+    getRecipes();
+  }, []);
+
+  //fetch call to API, log data
+  const getRecipes = async () => {
+    const response = await fetch(`https://api.edamam.com/search?q=chicken&app_id=${APP_ID}&app_key=${APP_KEY}&from=0&to=3&calories=591-722&health=alcohol-free
+    `);
+    const data = await response.json();
+    setRecipes(data.hits);
+  };
 
   return (
     <div className="App">
-      <h1>Hello React</h1>
+      <form className="search-form">
+        <input className="search-bar" type="text" />
+        <button className="search-button" type="submit">
+          Search
+        </button>
+      </form>
     </div>
   );
 };
